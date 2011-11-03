af0a770aacbb4a1d8beabda0f3f13bcbaeee1049
Hilmar Lapp <hlapp@drycafe.net>
2011-11-03 10:19:22 -0400

    Revert "Location of MOU has changed to a Google Doc." This reverts commit fe823b9fb23ee6b17d8f1909720e4e9fa5f07ffa. It's not the MOU that has changed, but the Acknowledgements. Fixing this.

--- src/org/nescent/plhdb/spring/SecurityServlet.java
+++ src/org/nescent/plhdb/spring/SecurityServlet.java
@@ -116,10 +116,10 @@ public class SecurityServlet extends DispatcherServlet {
 				super.doService(request, response);
 			}else if (uri.indexOf("MOU") > -1)
                             // FIXME: needs to be config parameter!
-				response.sendRedirect("https://docs.google.com/document/pub?id=1rjUon48QburrO-xHd_2qxiC9wMQwZf4fZQ9RdbalYtU");
+				response.sendRedirect("http://www.nescent.org/wg/plhd/images/d/d7/Final_Internal_MOU.pdf");
 			else if (uri.indexOf("Acknowledgments") > -1)
                             // FIXME: needs to be a config parameter!
-				response.sendRedirect("http://www.nescent.org/wg/plhd/images/f/f2/Acknowledgments.pdf");
+				response.sendRedirect("https://docs.google.com/document/pub?id=1rjUon48QburrO-xHd_2qxiC9wMQwZf4fZQ9RdbalYtU");
 			else if (uri.indexOf("register.go") > -1)
 				super.doService(request, response);
 			else if (uri.indexOf("cv.go") > -1)
