NESCent/plhdb

commit 9de2726ffd01ec12e97e8f878cc8d08cf870e7fe

Removes references to the Sf.net repo.
Closes #12.

WebRoot/tiles/aboutBody.jsp
@@ -1,4 +1,4 @@
-<h2>Data</h2>
+>Data</h2>
 <img src="/images/photos/bogart_copyright.jpg" style="float:right;margin-left: 15px; margin-bottom:15px;" />
 <p>
 This database contains  individual-based life 
@@ -27,7 +27,7 @@ for details</p>
 </ul>
 <h2>Source Code</h2>
 <p>
-Source code is available on <a href="http://github.com/plhdb/plhdb">GitHub</a> and <a href="http://sf.net/projects/plhdb">SourceForge</a> under a <a href="http://www.gnu.org/copyleft/gpl.html">GPL v3 license</a>. 
+Source code is available on <a href="http://github.com/NESCent/plhdb">GitHub</a> under a <a href="http://www.gnu.org/copyleft/gpl.html">GPL v3 license</a>. 
 </p> 
 <div style="clear:both;"></div>
 <h2>Credits</h2>

WebRoot/tiles/homeBody.jsp
@@ -47,7 +47,7 @@ our <a href="http://demo.plhdb.org">demo site</a>, which includes an example dat
 <li><a href="http://demo.plhdb.org">Demo site</a></li>
 <li><a href="https://www.nescent.org/wg_plhd">Project wiki site of the working group</a></li>
 <li><a href="http://www.nescent.org/science/awards_summary.php?id=19">NESCent award page of the working group</a></li>
-<li>Source code <a href="http://github.com/plhdb/plhdb">on GitHub</a> and <a href="http://sf.net/projects/plhdb">on SourceForge</a></li>
+<li>Source code <a href="http://github.com/NESCent/plhdb">on GitHub</a></li>
 <li><a href="http://www.nescent.org">National Evolutionary Synthesis Center</a> (NESCent)</li>
 </ul>
 </div>

commit 1db386c8bdc484353aea1b09b3415c7bc9f629f5

Corrects mistake introduced in 9de2726f.

WebRoot/tiles/aboutBody.jsp
@@ -1,4 +1,4 @@
->Data</h2>
+<h2>Data</h2>
 <img src="/images/photos/bogart_copyright.jpg" style="float:right;margin-left: 15px; margin-bottom:15px;" />
 <p>
 This database contains  individual-based life 